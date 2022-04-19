83e8c8d2d15a6f36edee4febfacde2b29ae32224
nabiladigo <nabiladigo91@gmail.com>
2022-04-19 16:59:41 -0400

    set up app.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  <Router>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
